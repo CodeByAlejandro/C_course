869b39ee9bb6c8934c80d9569bd86852389f7c75
Alejandro De Groote <CodeByAlejandro@gmail.com>
2025-06-11 22:54:05 +0200

    Updated Enums

--- Enums/src/main.c
+++ Enums/src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT = 100 };
-	enum Company xerox = XEROX, google = GOOGLE, ebay = EBAY, microsoft = MICROSOFT; 
-	printf("XEROX: %d\nGOOGLE: %d\nEBAY: %d\nMICROSOFT: %d\n\n", xerox, google, ebay, microsoft);
+	enum Company { GOOGLE, FACEBOOK, XEROX, YAHOO, EBAY, MICROSOFT = 100, NETFLIX };
+	enum Company xerox = XEROX,
+				 google = GOOGLE,
+				 ebay = EBAY,
+				 microsoft = MICROSOFT,
+				 netflix = NETFLIX;
+	printf("XEROX: %d\nGOOGLE: %d\nEBAY: %d\nMICROSOFT: %d\nNETFLIX: %d\n\n",
+			xerox, google, ebay, microsoft, netflix);
 	return 0;
 }
