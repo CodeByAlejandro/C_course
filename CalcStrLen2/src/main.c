8439b2287926cb7d5e00b6f9f0a500a8d274ec6b
Alejandro De Groote <CodeByAlejandro@gmail.com>
2025-06-20 00:05:37 +0200

    Added Calc Str Len 2

---
+++ CalcStrLen2/src/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int strLength(const char * string);
+
+int main(int argc, char **argv) {
+	char str1[] = "Alejandro";
+	char str2[] = "A";
+	char str3[] = "";
+	printf("The length of str1 is %d\n", strLength(str1));
+	printf("The length of str2 is %d\n", strLength(str2));
+	printf("The length of str3 is %d\n", strLength(str3));
+
+	return 0;
+}
+
+int strLength(const char * pString) {
+	const char * const pStartStr = pString;
+	for (; *pString; pString++);
+	return pString - pStartStr;
+}
