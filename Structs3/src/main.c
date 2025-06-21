a98b464734acca45c4a694aca90197150792eaa2
Alejandro De Groote <CodeByAlejandro@gmail.com>
2025-06-21 19:25:39 +0200

    Added Structs3

---
+++ Structs3/src/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+struct employee {
+	char name[50];
+	int hireDate;
+	float salary;
+};
+
+int main(int argc, char **argv) {
+
+	struct employee employee1 = { .hireDate = 21062025, .salary = 3536.37, .name = "Alejandro" };
+	
+	struct employee employee2;
+	printf("Enter employee data:\n");
+
+	printf("\tname: ");
+	fgets(employee2.name, sizeof(employee2.name), stdin);
+	if (ferror(stdin)) {
+		fprintf(stderr, "Failed to get employee name input!\n");
+		return 1;
+	}
+	// Allow EOF (CTRL + D, CTRL + D) to be valid input ending char (instead of newline)
+	clearerr(stdin);
+
+	// Remove ending newline from name
+	char * pChar = employee2.name;
+	for (; *pChar != '\n' && *pChar != '\0'; pChar++);
+	if (*pChar == '\n')
+		*pChar = '\0';
+	
+	printf("\thireDate: ");
+	scanf("%d", &employee2.hireDate);
+	// Allow EOF (CTRL + D, CTRL + D) to be valid input ending char (instead of newline)
+	clearerr(stdin);
+	printf("\tsalary: ");
+	scanf("%f", &employee2.salary);
+	// Allow EOF (CTRL + D, CTRL + D) to be valid input ending char (instead of newline)
+	clearerr(stdin);
+	
+	printf("\n");
+	
+	printf("List of employees:\n");
+	printf("\temployee1:\n");
+	printf("\t\tname: %s\n", employee1.name);
+	printf("\t\thireDate: %d\n", employee1.hireDate);
+	printf("\t\tsalary: %.2f\n", employee1.salary);
+	printf("\temployee2:\n");
+	printf("\t\tname: %s\n", employee2.name);
+	printf("\t\thireDate: %d\n", employee2.hireDate);
+	printf("\t\tsalary: %.2f\n", employee2.salary);
+	
+	printf("\n");
+	return 0;
+}
