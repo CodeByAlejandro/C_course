1f8a8967a39bb0e2ffa0feaefef6a760d3d64bb2
Alejandro De Groote <CodeByAlejandro@gmail.com>
2025-07-16 01:18:44 +0200

    Added comments to script for Dynamic Loading

--- DynamicLoading/src/dynamicLoad.sh
+++ DynamicLoading/src/dynamicLoad.sh
@@ -1,2 +1,4 @@
 gcc string_functions.c -fPIC -shared -o libstring_functions.so
 gcc dynamicLoading.c -ldl -o main
+# No need to change LD_LIBRARY_PATH here since an absolute path is used in the dlopen() call
+# (and the libdl library for the dynamic loading itself is present in a default system lib dir)
