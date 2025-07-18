5e208d72a83fca86512ab1a0b54393d78fb40f20
Alejandro De Groote <CodeByAlejandro@gmail.com>
2025-07-19 01:26:00 +0200

    Added comments to Mocking

--- Mocking/include/mocking.h
+++ Mocking/include/mocking.h
@@ -10,7 +10,29 @@ struct found_mock {
     struct mock mock;
 };
 
+/*
+ * Use this function to set a mock in test code (at runtime), for a function wrapped by the MOCK_FOR_TEST-macro in non-test code.
+ * Simply do not use this function, if you still want to run the real function call, in a particular test.
+ */
 void set_mock(const void * func, void * val);
+
+/*
+ * Function for use in the MOCK_FOR_TEST-macro that is expanded in non-test code.
+ * It is used to retrieve the mocked value set by the set_mock-function in a test.
+ * The returned struct contains a bool found to indicate whether a mock was found and thus whether the mocked value or the real call should be used.
+ */
 struct found_mock * get_mock(const void * func);
+
+/*
+ * Use this function to remove a previously set mock in test code (at runtime), for a function wrapped by the MOCK_FOR_TEST-macro in non-test code.
+ */
 void remove_mock(const void * func);
+
+/*
+ * Use this function to remove all previously set mocks in test code (at runtime), for functions wrapped by the MOCK_FOR_TEST-macro in non-test code.
+ * It will deallocate all the dynamically allocated mock list memory.
+ * 
+ * Note: If you don't call this function, and if you don't remove all previously set mocks 1 by 1 with the remove_mock function manually, you will technically create a memory leak.
+ * This is not such a big deal though as the mock list is to be used during the entire lifetime of the test execution anyway.
+ */
 void reset_mocks(void);
--- Mocking/include/testable.h
+++ Mocking/include/testable.h
@@ -3,9 +3,24 @@
 #include "mocking.h"
 
 #ifdef TEST
+    /*
+     * Sets static methods to be extern in TEST-mode to allow testing from a separate source file.
+     */
     #define EXTERN_FOR_TEST 
+    /*
+     * Use this macro in non-test code to allow mocking in TEST mode.
+     * If no mock is found at runtime the real call is used as fallback.
+     *
+     * Note: the get_mock function is called twice but the return value is cached.
+     */
     #define MOCK_FOR_TEST(real_call, func, return_type) (((get_mock((void *) func))->found) ? *((return_type *) ((get_mock((void *) func))->mock.val)) : real_call)
 #else
+    /*
+     * Resolves to static since TEST-mode is off.
+     */
     #define EXTERN_FOR_TEST static
+    /*
+     * Runs the real call since TEST mode is off.
+     */
     #define MOCK_FOR_TEST(real_call, func, return_type) real_call
 #endif
